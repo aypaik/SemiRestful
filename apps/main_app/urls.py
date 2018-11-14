3471357fe007cc7e82f9b810a148012d4a0c2774
aypaik <32079942+aypaik@users.noreply.github.com>
2018-11-14 13:59:46 -0800

    Add files via upload

---
+++ apps/main_app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.http import HttpResponse
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^add/?$', views.add),
+    url(r'^create/?$', views.create),
+    url(r'^show/?(?P<user_id>\d+)$', views.show),
+    url(r'^edit/?(?P<user_id>\d+)$', views.edit),
+    url(r'^update/?(?P<user_id>\d+)$', views.update),
+    url(r'^delete/?(?P<user_id>\d+)$', views.delete),
+]
